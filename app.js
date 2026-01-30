fe47976b5504831e004ddbb727b771ea9d7c37b0
heilieger <88578623+heilieger@users.noreply.github.com>
2026-01-30 21:10:52 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,6 @@
+body { background: #333; display: flex; justify-content: center; align-items: center; height: 100vh; margin: 0; font-family: sans-serif; }
+.menu-card { background: white; border: 15px solid #ff0000; border-radius: 40px; padding: 50px; text-align: center; width: 80%; }
+h1 { font-size: 3rem; margin: 0; color: #444; }
+h2 { font-size: 5rem; color: #ff0000; margin: 10px 0; }
+.date-box { font-size: 2.5rem; color: #888; margin-bottom: 30px; }
+.menu-box { font-size: 4rem; line-height: 1.3; font-weight: bold; color: #222; }
